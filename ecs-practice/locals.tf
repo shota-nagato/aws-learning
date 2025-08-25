d7660a61c34811f9e7aeeab25dd890d1a9360a21
shota-nagato <shota.nagato@gmail.com>
2025-08-25 22:03:49 +0900

    networkのvariable作成

--- ecs-practice/locals.tf
+++ ecs-practice/locals.tf
@@ -4,4 +4,38 @@ locals {
     env    = "dev"
     region = "ap-northeast-1"
   }
+
+  network = {
+    cidr = "172.16.0.0/16"
+    subnet_groups = {
+      public_ingress = {
+        visibility = "public"
+        tier       = "ingress"
+        subnets = [
+          {
+            az   = "a"
+            cidr = "172.16.1.0/24"
+          },
+          {
+            az   = "c"
+            cidr = "172.16.2.0/24"
+          }
+        ]
+      }
+      private_container = {
+        visibility = "private"
+        tier       = "container"
+        subnets = [
+          {
+            az   = "a"
+            cidr = "172.16.3.0/24"
+          },
+          {
+            az   = "c"
+            cidr = "172.16.4.0/24"
+          }
+        ]
+      }
+    }
+  }
 }
--- modules/network/main.tf
+++ modules/network/main.tf
@@ -1,9 +1,34 @@
 resource "aws_vpc" "main" {
-  cidr_block           = "172.16.0.0/16"
+  cidr_block           = var.network.cidr
   enable_dns_hostnames = true
   enable_dns_support   = true
 
   tags = {
     Name = "${var.common.prefix}-${var.common.env}-vpc"
+  }
+}
+
+locals {
+  subnets = merge([
+    for group_name, group in var.network.subnet_groups : {
+      for subnet in group.subnets : "${group_name}-${subnet.az}" => {
+        type = group.visibility
+        tier = group.tier
+        az   = subnet.az
+        cidr = subnet.cidr
+      }
+    }
+  ]...)
+}
+
+resource "aws_subnet" "subnets" {
+  for_each = local.subnets
+
+  vpc_id            = aws_vpc.main.id
+  availability_zone = "${var.common.region}${each.value.az}"
+  cidr_block        = each.value.cidr
+
+  tags = {
+    Name = "${var.common.prefix}-${var.common.env}-${each.value.type}-${each.value.tier}-subnet-1${each.value.az}"
   }
 }
