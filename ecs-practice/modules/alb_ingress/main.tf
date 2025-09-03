87d4c00aa339609281609694e8510eed9c0fc6ae
shota-nagato <shota.nagato@gmail.com>
2025-09-03 23:19:49 +0900

    loadbalancerを扱うためのロールを作成

--- ecs-practice/modules/alb_ingress/main.tf
+++ ecs-practice/modules/alb_ingress/main.tf
@@ -27,13 +27,52 @@ resource "aws_lb_target_group" "blue" {
   }
 }
 
-resource "aws_lb_listener" "ingress" {
+resource "aws_lb_target_group" "green" {
+  name        = "${var.common.prefix}-alb-tg-green"
+  target_type = "ip"
+  protocol    = "HTTP"
+  port        = 80
+  vpc_id      = var.network.vpc_id
+
+  health_check {
+    path                = "/"
+    protocol            = "HTTP"
+    port                = "traffic-port"
+    timeout             = 5
+    interval            = 30
+    healthy_threshold   = 5
+    unhealthy_threshold = 2
+  }
+}
+
+resource "aws_lb_listener" "blue" {
   load_balancer_arn = aws_lb.ingress.arn
   protocol          = "HTTP"
   port              = 80
 
   default_action {
+    type = "forward"
+
+    forward {
+      target_group {
+        arn    = aws_lb_target_group.blue.arn
+        weight = 100
+      }
+      target_group {
+        arn    = aws_lb_target_group.green.arn
+        weight = 0
+      }
+    }
+  }
+}
+
+resource "aws_lb_listener" "green" {
+  load_balancer_arn = aws_lb.ingress.arn
+  protocol          = "HTTP"
+  port              = 9000
+
+  default_action {
     type             = "forward"
-    target_group_arn = aws_lb_target_group.blue.arn
+    target_group_arn = aws_lb_target_group.green.arn
   }
 }
--- ecs-practice/modules/ecs/main.tf
+++ ecs-practice/modules/ecs/main.tf
@@ -70,3 +70,25 @@ resource "aws_iam_role_policy_attachment" "ecs_exec_task_execution" {
   role       = aws_iam_role.task_execution_role.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
 }
+
+resource "aws_iam_role" "ecs_infrastructure_role_for_load_balancers" {
+  name = "ecsInfrastructureRoleForLoadBalancers"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        "Sid" : "AllowAccessToECSForInfrastructureManagement",
+        "Effect" : "Allow",
+        "Principal" : {
+          "Service" : "ecs.amazonaws.com"
+        },
+        "Action" : "sts:AssumeRole"
+      }
+    ]
+  })
+}
+resource "aws_iam_role_policy_attachment" "ecs_infrastructure_role_for_load_balancers_attachment" {
+  role       = aws_iam_role.ecs_infrastructure_role_for_load_balancers.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonECSInfrastructureRolePolicyForLoadBalancers"
+}
