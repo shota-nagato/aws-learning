8f72dbf41f974e51d98267322e7d0b0cc4e1446c
shota-nagato <shota.nagato@gmail.com>
2025-10-25 14:09:41 +0900

    policyの適応漏れ

--- nextjs-ssg/modules/s3/main.tf
+++ nextjs-ssg/modules/s3/main.tf
@@ -33,3 +33,9 @@ data "aws_iam_policy_document" "s3_access" {
     }
   }
 }
+
+resource "aws_s3_bucket_policy" "next" {
+  bucket = aws_s3_bucket.next.id
+  policy = data.aws_iam_policy_document.s3_access.json
+}
+
